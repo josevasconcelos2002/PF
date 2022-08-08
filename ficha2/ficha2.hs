129b035e382525f22d818ab58f0e8423a9515e84
José Vasconcelos <zevasconcelos6@gmail.com>
2022-08-08 12:03:54 +0100

    Alguns exercícios da ficha 2, feitos

---
+++ ficha2/ficha2.hs
@@ -0,0 +1,68 @@
+module Ficha2 where
+
+
+
+-- 2
+
+-- a)
+
+dobros :: [Float] -> [Float]
+dobros [] = []
+dobros (x:xs) = 2*x : dobros xs
+
+-- b)
+
+numOcorre :: Char -> String -> Int
+numOcorre ' ' _ = 0
+numOcorre a (x:xs) | a == x = 1 + numOcorre a xs
+                   | otherwise = numOcorre a xs
+
+-- c)
+
+positivos :: [Int] -> Bool
+positivos [] = False
+positivos (x:xs) | x < 0 = False
+                 | otherwise = True && positivos xs
+
+-- d)
+
+soPos :: [Int] -> [Int]
+soPos [] = []
+soPos (x:xs) | x < 0 = soPos xs
+             | otherwise = x : soPos xs
+
+-- e)
+
+somaNeg :: [Int] -> Int
+somaNeg [] = 0
+somaNeg (x:xs) | x < 0 = x + somaNeg xs
+               | otherwise = somaNeg xs
+
+
+-- f)
+{-
+tresUlt :: [a] -> [a]
+tresUlt [] = []
+tresUlt l | length l < 4 = l
+          | otherwise =                  -- ?
+-}
+
+-- g)
+
+segundos :: [(a,b)] -> [b]
+segundos [] = []
+segundos ((x,xs):h) = xs : segundos h 
+
+
+-- h)
+
+nosPrimeiros :: (Eq a) => a -> [(a,b)] -> Bool
+nosPrimeiros _ [] = False
+nosPrimeiros a ((x,xs):h) | a == x = True
+                          | otherwise = False || nosPrimeiros a h
+
+
+-- i)
+
+sumTriplos :: (Num a, Num b, Num c) => [(a,b,c)] -> (a,b,c)
+sumTriplos ((a,b,c),(d,e,f)):h = (a+d,b+e,) 
